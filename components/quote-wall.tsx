072dfc7f80c55db08d03f8bc29bf29c965382020
Kaleb A <kalebarora@outlook.com>
2024-12-30 12:25:51 -0500

    Update quote-wall.tsx

--- components/quote-wall.tsx
+++ components/quote-wall.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { motion } from 'framer-motion'
 
 interface Quote {
